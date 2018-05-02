6e0a5f886528a966d5fe1dde6789e05ecd634fa4
Jóakim von Kistowski <joakim.kistowski@uni-wuerzburg.de>
2018-05-02 15:35:55 +0200

    function signatures and doc

--- brainsimulation.h
+++ brainsimulation.h
@@ -1,11 +1,53 @@
 //types
+
+/**
+ * Type that the nodes in the brainsimulation use to store their energy level.
+ */
 typedef double t_nodeval;
 
+/**
+ * Struct to store the results of a simulation for a single observed node.
+ * x_index and y_index members must be set when passing it to a simulation.
+ */
 typedef struct 
 {
-	int index;
+	/**
+	 * x index of the node.
+	 */
+	int x_index;
+	/**
+	 * y index of the node.
+	 */
+	int y_index;
+	/**
+	 * Series of observed node energy levels. One element per tick.
+	 * Has #timeseries_ticks as length.
+	 */
 	t_nodeval * timeseries;
-	int timeseries_length;
+	/**
+	 * Length of #timeseries.
+	 */
+	int timeseries_ticks;
 } t_nodetimeseries;
 
 //function declarations
+
+/**
+ * Simulates the brain.
+ * @param tick_ms Milliseconds in between each simulation tick.
+ * @param num_ticks The number of ticks to simulate.
+ * This will also be the length of all timeseries contained in the returned
+ * oberservationnodes.
+ * @param nodes 2D array of nodes with their starting energy level.
+ * @param num_obervationnodes The number of nodes to observe during simulation.
+ * @param The nodes to observe during simulation. x_index and y_index members
+ * must be set. All other members will be overwritten with the simulation
+ * results. Must have observationnodes as length.
+ @return Return-codes.
+ */
+int simulate(int tick_ms,
+	int num_ticks,
+	t_nodeval ** nodes,
+	int num_obervationnodes,
+	t_nodetimeseries * oberservationnodes);
+	
