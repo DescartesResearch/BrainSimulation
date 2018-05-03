83381f954872a6a97c51ecca5477df6bb98dc73f
Johannes Grohmann <johannes.grohmann@uni-wuerzburg.de>
2018-05-03 15:18:32 +0200

    fixed syntax errors

--- brainsimulation.c
+++ brainsimulation.c
@@ -1,17 +1,16 @@
 #include "brainsimulation.h"
-#include "nodefunc.h"
-
+#include <stdio.h>
 // implement the actual simulation here
-int simulate(int tick_ms, int num_ticks, t_nodeval ** nodes, int num_obervationnodes,
-             t_nodetimeseries * oberservationnodes){
+int simulate(int tick_ms, int num_ticks, int number_nodes_x, int number_nodes_y, t_nodeval ** nodes,
+             int num_obervationnodes, t_nodetimeseries * observationnodes){
     printf("Starting simulation.\n");
     printf("Number of ticks: %d\n", num_ticks);
-    printf("Length of each tick (ms): %d\n", tickms);
+    printf("Length of each tick (ms): %d\n", tick_ms);
     printf("Number of observation nodes: %d\n", num_obervationnodes);
     printf("Observation nodes: %d\n", num_obervationnodes);
     int i = 0;
     for (i = 0; i < num_obervationnodes; ++i) {
-        printf("(%d;%d), ", obervationnodes[i].x_index, obervationnodes[i].y_index);
+        printf("(%d;%d), ", observationnodes[i].x_index, observationnodes[i].y_index);
     }
     printf("\n");
     return 0;
